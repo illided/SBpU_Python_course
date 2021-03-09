bfee0bc21dd3e8e127550be89932271be608f335
illided <utkin.ilya.nickolaevich@gmail.com>
2021-03-09 15:56:53 +0300

    more tests and some refactoring

--- solutions/hw2/task2/cacher.py
+++ solutions/hw2/task2/cacher.py
@@ -2,7 +2,7 @@ from typing import List, Any, Tuple, Callable
 import functools
 
 
-class FunctionCache:
+class FunctionCallCache:
     nameless_args: Tuple
     named_args: dict
     result: Any
@@ -16,10 +16,10 @@ class FunctionCache:
         return f"Given: ({self.nameless_args}, {self.named_args}) Returned: {self.result}"
 
 
-class _FunctionResultCacher:
+class _FunctionCallsCacher:
     function: Callable
     maximum_num_of_caches: int
-    cache: List[FunctionCache]
+    cache: List[FunctionCallCache]
 
     def __init__(self, function=None, *, maximum_num_of_caches):
         if maximum_num_of_caches < 1:
@@ -35,25 +35,28 @@ class _FunctionResultCacher:
         result = self.function(*args, **kwargs)
         if self.is_cache_full():
             self.cache.pop(0)
-        self.cache.append(FunctionCache(args, kwargs, result))
+        self.cache.append(FunctionCallCache(args, kwargs, result))
         return result
 
-    def is_cache_full(self):
+    def is_cache_full(self) -> bool:
         return len(self.cache) == self.maximum_num_of_caches
 
-    def get_cache(self):
+    def get_cache(self) -> List[str]:
         return [str(c) for c in self.cache]
 
     def clear_cache(self):
         self.cache.clear()
 
+    def last_call_info(self) -> str:
+        return str(self.cache[-1])
+
 
 def FunctionCacher(function=None, *, maximum_num_of_caches: int):
     if function:
-        return _FunctionResultCacher(function, maximum_num_of_caches=maximum_num_of_caches)
+        return _FunctionCallsCacher(function, maximum_num_of_caches=maximum_num_of_caches)
     else:
 
         def wrapper(func):
-            return _FunctionResultCacher(func, maximum_num_of_caches=maximum_num_of_caches)
+            return _FunctionCallsCacher(func, maximum_num_of_caches=maximum_num_of_caches)
 
         return wrapper
--- tests/hw2/task2/test_cacher.py
+++ tests/hw2/task2/test_cacher.py
@@ -9,7 +9,7 @@ def sum_two_numbers(a: int, b: int):
 
 @FunctionCacher(maximum_num_of_caches=2)
 def sum_arbitrary_amount_of_numbers(*args):
-    return sum([args])
+    return sum(args)
 
 
 @FunctionCacher(maximum_num_of_caches=2)
@@ -17,15 +17,69 @@ def join_to_string(separator: str, *args):
     return separator.join(args)
 
 
-class CacherCashContentTests(unittest.TestCase):
+@FunctionCacher(maximum_num_of_caches=2)
+def end_string(string: str, ending: str = "."):
+    return string + ending
+
+
+class CacherTests(unittest.TestCase):
     def tearDown(self) -> None:
         sum_two_numbers.clear_cache()
         sum_arbitrary_amount_of_numbers.clear_cache()
         join_to_string.clear_cache()
+        end_string.clear_cache()
+
+    def test_calling_with_zero_cache(self):
+        self.assertEqual(0, len(end_string.get_cache()))
 
     def test_with_two_positional_arguments(self):
         sum_two_numbers(4, 5)
-        self.assertEqual("Given: ((4, 5), {}) Returned: 9", sum_two_numbers.get_cache()[0])
+        self.assertEqual("Given: ((4, 5), {}) Returned: 9", sum_two_numbers.last_call_info())
+
+    def test_with_two_named_arguments(self):
+        sum_two_numbers(a=4, b=5)
+        self.assertEqual("Given: ((), {'a': 4, 'b': 5}) Returned: 9", sum_two_numbers.last_call_info())
+
+    def test_with_arbitrary_num_of_arguments(self):
+        sum_arbitrary_amount_of_numbers(1, 2, 3, 4, 5)
+        self.assertEqual("Given: ((1, 2, 3, 4, 5), {}) Returned: 15", sum_arbitrary_amount_of_numbers.last_call_info())
+
+    def test_with_one_positional_and_some_optional_arguments(self):
+        join_to_string(".", "hello", "world")
+        self.assertEqual("Given: (('.', 'hello', 'world'), {}) Returned: hello.world", join_to_string.last_call_info())
+
+    def test_with_argument_by_default(self):
+        end_string("hello")
+        self.assertEqual("Given: (('hello',), {}) Returned: hello.", end_string.last_call_info())
+
+    def test_with_unnamed_argument_by_default_but_filled_manually(self):
+        end_string("hello", " world!")
+        self.assertEqual("Given: (('hello', ' world!'), {}) Returned: hello world!", end_string.last_call_info())
+
+    def test_with_named_argument_by_default_but_filled_manually(self):
+        end_string("hello", ending=" world!")
+        self.assertEqual(
+            "Given: (('hello',), {'ending': ' world!'}) Returned: hello world!", end_string.last_call_info()
+        )
+
+    def test_getting_correct_num_of_cache_values(self):
+        for i in range(10):
+            sum_two_numbers(i, i)
+        self.assertEqual(2, len(sum_two_numbers.get_cache()))
+
+    def test_trying_to_call_with_negative_num_of_cache_calls(self):
+        with self.assertRaises(TypeError):
+
+            @FunctionCacher(maximum_num_of_caches=-100)
+            def inner():
+                return "hello"
+
+    def test_trying_to_call_with_zero_maximum_num_of_cache(self):
+        with self.assertRaises(TypeError):
+
+            @FunctionCacher(maximum_num_of_caches=0)
+            def inner():
+                return "hello"
 
 
 if __name__ == "__main__":
