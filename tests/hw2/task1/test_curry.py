25ae0763ce096311e6dd5551fae8068b73c343fd
illided <utkin.ilya.nickolaevich@gmail.com>
2021-03-09 11:18:48 +0300

    tests with infinite arity added

--- tests/hw2/task1/test_curry.py
+++ tests/hw2/task1/test_curry.py
@@ -3,7 +3,7 @@ import unittest
 from solutions.hw2.task1.curry import curry_explicit, uncurry_explicit
 
 
-def f(x, y, z):
+def return_three_things_in_a_string(x, y, z):
     return f"{[x, y, z]}"
 
 
@@ -15,18 +15,25 @@ def return_eight():
     return 8
 
 
+def multiply_all(*args):
+    result = 1
+    for x in args:
+        result = result * x
+    return result
+
+
 class CurryingTest(unittest.TestCase):
     def test_correct_currying(self):
-        g = curry_explicit(f, 3)
+        g = curry_explicit(return_three_things_in_a_string, 3)
         self.assertEqual("[1, 2, 3]", g(1)(2)(3))
 
     def test_too_small_currying(self):
-        g = curry_explicit(f, 2)
+        g = curry_explicit(return_three_things_in_a_string, 2)
         with self.assertRaises(TypeError):
             g(1)(2)(3)
 
     def test_too_big_currying(self):
-        g = curry_explicit(f, 5)
+        g = curry_explicit(return_three_things_in_a_string, 5)
         with self.assertRaises(TypeError):
             g(1)(2)(3)(4)(5)
 
@@ -45,25 +52,35 @@ class CurryingTest(unittest.TestCase):
 
     def test_calling_with_negative_currying(self):
         with self.assertRaises(TypeError):
-            curry_explicit(f, -100)
+            curry_explicit(return_three_things_in_a_string, -100)
+
+    def test_infinite_arity(self):
+        curried_product_func = curry_explicit(multiply_all, 3)
+        self.assertEqual(6, curried_product_func(1)(2)(3))
 
 
 class UncurryTest(unittest.TestCase):
-    def test_uncurry_with_no_curried_args(self):
-        g = curry_explicit(f, 3)
+    def test_with_no_curried_args(self):
+        g = curry_explicit(return_three_things_in_a_string, 3)
         h = uncurry_explicit(g, 3)
         self.assertEqual("[1, 2, 3]", h(1, 2, 3))
 
-    def test_uncurry_with_curried_args(self):
-        g = curry_explicit(f, 3)(1)(2)
+    def test_with_curried_args(self):
+        g = curry_explicit(return_three_things_in_a_string, 3)(1)(2)
         h = uncurry_explicit(g, 1)
         self.assertEqual("[1, 2, 3]", h(3))
 
-    def test_uncurry_declared_arity_too_big(self):
-        g = curry_explicit(f, 3)
+    def test_declared_arity_too_big(self):
+        g = curry_explicit(return_three_things_in_a_string, 3)
         h = uncurry_explicit(g, 5)
         with self.assertRaises(TypeError):
             h(1, 2, 3, 4, 5)
+
+    def test_infinite_arity(self):
+        curried_product_func = curry_explicit(multiply_all, 3)
+        uncurried_product_func = uncurry_explicit(curried_product_func, 3)
+        with self.assertRaises(TypeError):
+            uncurried_product_func(1, 2, 3, 4)
 
 
 if __name__ == "__main__":
