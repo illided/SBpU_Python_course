254708c8d69a6900f5743919941b2a212f3cc854
illided <utkin.ilya.nickolaevich@gmail.com>
2021-03-11 20:36:10 +0300

    solution bugs refactored and more tests added

--- solutions/hw2/task2/cacher.py
+++ solutions/hw2/task2/cacher.py
@@ -1,62 +1,70 @@
-from typing import List, Any, Tuple, Callable
+from typing import List, Any, Tuple, Callable, FrozenSet
+from collections import OrderedDict
 import functools
-
-
-class FunctionCallCache:
-    nameless_args: Tuple
-    named_args: dict
-    result: Any
-
-    def __init__(self, args: Tuple, kwargs: dict, result):
-        self.nameless_args = args
-        self.named_args = kwargs
-        self.result = result
-
-    def __str__(self):
-        return f"Given: ({self.nameless_args}, {self.named_args}) Returned: {self.result}"
-
-
-class _FunctionCallsCacher:
-    function: Callable
-    maximum_num_of_caches: int
-    cache: List[FunctionCallCache]
-
-    def __init__(self, function=None, *, maximum_num_of_caches):
-        if maximum_num_of_caches < 1:
-            raise TypeError("Can't have empty cache")
-
-        self.function = function
-        self.maximum_num_of_caches = maximum_num_of_caches
-        self.cache = []
-
-        functools.update_wrapper(self, function)
-
-    def __call__(self, *args, **kwargs):
-        result = self.function(*args, **kwargs)
-        if self.is_cache_full():
-            self.cache.pop(0)
-        self.cache.append(FunctionCallCache(args, kwargs, result))
-        return result
-
-    def is_cache_full(self) -> bool:
-        return len(self.cache) == self.maximum_num_of_caches
-
-    def get_cache(self) -> List[str]:
-        return [str(c) for c in self.cache]
-
-    def clear_cache(self):
-        self.cache.clear()
-
-    def last_call_info(self) -> str:
-        return str(self.cache[-1])
 
 
 def FunctionCacher(function=None, *, maximum_num_of_caches: int):
     if function:
         return _FunctionCallsCacher(function, maximum_num_of_caches=maximum_num_of_caches)
-    else:
 
-        def wrapper(func):
-            return _FunctionCallsCacher(func, maximum_num_of_caches=maximum_num_of_caches)
+    def wrapper(func):
+        return _FunctionCallsCacher(func, maximum_num_of_caches=maximum_num_of_caches)
 
-        return wrapper
+    return wrapper
+
+
+class FunctionCallArgumentsCache:
+    nameless_args: Tuple
+    named_args: frozenset
+
+    def __init__(self, args: Tuple, kwargs: dict):
+        self.nameless_args = args
+        self.named_args = frozenset(kwargs.items())
+
+    def __hash__(self):
+        return hash((self.named_args, self.nameless_args))
+
+    def __eq__(self, other):
+        if not isinstance(other, FunctionCallArgumentsCache):
+            raise NotImplementedError("Can't compare function call argument cache to other objects")
+        return self.named_args == other.named_args and self.nameless_args == other.nameless_args
+
+
+class _FunctionCallsCacher:
+    function: Callable
+    maximum_num_of_caches: int
+    cache: 'OrderedDict[FunctionCallArgumentsCache, Any]'
+
+    def __init__(self, function=None, *, maximum_num_of_caches=0):
+        if maximum_num_of_caches < 0:
+            raise TypeError("Can't have empty cache")
+
+        self.function = function
+        self.maximum_num_of_caches = maximum_num_of_caches
+        self.cache = OrderedDict()
+        functools.update_wrapper(self, function)
+
+    def __call__(self, *args, **kwargs):
+        result = self.function(*args, **kwargs)
+        if self.maximum_num_of_caches != 0:
+            if self.is_cache_full():
+                self.cache.popitem(last=False)
+            self.cache[FunctionCallArgumentsCache(args, kwargs)] = result
+        return result
+
+    def is_cache_full(self) -> bool:
+        return len(self.cache) == self.maximum_num_of_caches
+
+    def get_cached_result(self, *args, **kwargs):
+        if not self.is_such_call_cached(args, kwargs):
+            raise ValueError("Call with such arguments is not cached")
+        return self.cache[FunctionCallArgumentsCache(args, kwargs)]
+
+    def is_such_call_cached(self, args, kwargs):
+        return FunctionCallArgumentsCache(args, kwargs) in self.cache
+
+    def clear_cache(self):
+        self.cache.clear()
+
+    def last_call_info(self):
+        return next(reversed(self.cache.items()))
--- tests/hw2/task2/test_cacher.py
+++ tests/hw2/task2/test_cacher.py
@@ -22,7 +22,7 @@ def end_string(string: str, ending: str = "."):
     return string + ending
 
 
-class CacherTests(unittest.TestCase):
+class CacherTestsForOneCall(unittest.TestCase):
     def tearDown(self) -> None:
         sum_two_numbers.clear_cache()
         sum_arbitrary_amount_of_numbers.clear_cache()
@@ -30,42 +30,36 @@ class CacherTests(unittest.TestCase):
         end_string.clear_cache()
 
     def test_calling_with_zero_cache(self):
-        self.assertEqual(0, len(end_string.get_cache()))
+        with self.assertRaises(ValueError):
+            sum_two_numbers.get_cached_result(1, 2)
 
     def test_with_two_positional_arguments(self):
         sum_two_numbers(4, 5)
-        self.assertEqual("Given: ((4, 5), {}) Returned: 9", sum_two_numbers.last_call_info())
+        self.assertEqual(9, sum_two_numbers.get_cached_result(4, 5))
 
     def test_with_two_named_arguments(self):
         sum_two_numbers(a=4, b=5)
-        self.assertEqual("Given: ((), {'a': 4, 'b': 5}) Returned: 9", sum_two_numbers.last_call_info())
+        self.assertEqual(9, sum_two_numbers.get_cached_result(a=4, b=5))
 
     def test_with_arbitrary_num_of_arguments(self):
         sum_arbitrary_amount_of_numbers(1, 2, 3, 4, 5)
-        self.assertEqual("Given: ((1, 2, 3, 4, 5), {}) Returned: 15", sum_arbitrary_amount_of_numbers.last_call_info())
+        self.assertEqual(15, sum_arbitrary_amount_of_numbers.get_cached_result(1, 2, 3, 4, 5))
 
     def test_with_one_positional_and_some_optional_arguments(self):
         join_to_string(".", "hello", "world")
-        self.assertEqual("Given: (('.', 'hello', 'world'), {}) Returned: hello.world", join_to_string.last_call_info())
+        self.assertEqual("hello.world", join_to_string.get_cached_result(".", "hello", "world"))
 
     def test_with_argument_by_default(self):
         end_string("hello")
-        self.assertEqual("Given: (('hello',), {}) Returned: hello.", end_string.last_call_info())
+        self.assertEqual("hello.", end_string.get_cached_result("hello"))
 
     def test_with_unnamed_argument_by_default_but_filled_manually(self):
         end_string("hello", " world!")
-        self.assertEqual("Given: (('hello', ' world!'), {}) Returned: hello world!", end_string.last_call_info())
+        self.assertEqual("hello world!", end_string.get_cached_result("hello", " world!"))
 
     def test_with_named_argument_by_default_but_filled_manually(self):
         end_string("hello", ending=" world!")
-        self.assertEqual(
-            "Given: (('hello',), {'ending': ' world!'}) Returned: hello world!", end_string.last_call_info()
-        )
-
-    def test_getting_correct_num_of_cache_values(self):
-        for i in range(10):
-            sum_two_numbers(i, i)
-        self.assertEqual(2, len(sum_two_numbers.get_cache()))
+        self.assertEqual("hello world!", end_string.get_cached_result("hello", ending=" world!"))
 
     def test_trying_to_call_with_negative_num_of_cache_calls(self):
         with self.assertRaises(TypeError):
@@ -74,12 +68,23 @@ class CacherTests(unittest.TestCase):
             def inner():
                 return "hello"
 
-    def test_trying_to_call_with_zero_maximum_num_of_cache(self):
-        with self.assertRaises(TypeError):
 
-            @FunctionCacher(maximum_num_of_caches=0)
-            def inner():
-                return "hello"
+class CacherTestsArbitraryNumOfCalls(unittest.TestCase):
+    def test_getting_correct_num_of_cache_values(self):
+        sum_two_numbers(0, 0)
+        for i in range(10):
+            sum_two_numbers(i, i)
+        with self.assertRaises(ValueError):
+            sum_two_numbers.get_cached_result(0, 0)
+
+    def test_trying_to_call_with_zero_maximum_num_of_cache(self):
+        @FunctionCacher(maximum_num_of_caches=0)
+        def multiply(a: int, b: int):
+            return a*b
+
+        multiply(1, 2)
+        with self.assertRaises(ValueError):
+            multiply.get_cached_result(1, 2)
 
 
 if __name__ == "__main__":
