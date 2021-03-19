303a482ed7d471a1dbf0492f0c1892096a9f5ed2
illided <utkin.ilya.nickolaevich@gmail.com>
2021-03-19 17:57:57 +0300

    more tests added

--- tests/test1/task1/test_spy.py
+++ tests/test1/task1/test_spy.py
@@ -12,6 +12,9 @@ def return_something_or_eight(x=None):
     if x is None:
         return 8
 
+@Spy
+def return_sum(x, y):
+    return x+y
 
 class SpyTest(unittest.TestCase):
     def tearDown(self) -> None:
@@ -32,3 +35,13 @@ class SpyTest(unittest.TestCase):
         return_something_or_eight(x=10)
         statistic = [y for x, y in print_usage_statistic(return_something_or_eight)]
         self.assertEqual({"x": 10}, statistic[0])
+
+    def test_many_arguments(self):
+        return_sum(7, 9)
+        statistic = [y for x, y in print_usage_statistic(return_sum)]
+        self.assertEqual({"x" : 7, "y" : 9}, statistic[0])
+
+    def test_many_arguments_filled_backwards(self):
+        return_sum(y=9, x=7)
+        statistic = [y for x, y in print_usage_statistic(return_sum)]
+        self.assertEqual({"x": 7, "y": 9}, statistic[0])
