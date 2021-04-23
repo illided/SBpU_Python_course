0f636bf82a62c9f3d3bd6f405d8c49f0e9875953
illided <utkin.ilya.nickolaevich@gmail.com>
2021-04-23 18:21:53 +0300

    some refactor

--- solutions/test1/task3/main.py
+++ solutions/test1/task3/main.py
@@ -3,10 +3,13 @@ from statistic import FileStatistic
 
 def main():
     stat = FileStatistic("Alice_in_wonderland.txt")
-    print("Top 10 words:")
-    for el in stat.get_most_common():
-        print(f"{el[0]} : {el[1]}")
-    print(f"Num of sentences in text: {stat.get_num_of_sentences()}")
+    open("stat.txt", 'w').close()
+    with open("stat.txt", 'a') as stat_output:
+        stat_output.write("Top 10 words:\n")
+        for el in stat.get_most_common():
+            stat_output.write(f"{el[0]} : {el[1]}\n")
+        stat_output.write(f"Num of sentences in text: {stat.get_num_of_sentences()}\n")
+
 
 
 if __name__ == '__main__':
--- solutions/test1/task3/statistic.py
+++ solutions/test1/task3/statistic.py
@@ -14,15 +14,24 @@ class FileStatistic:
         self.punc_count = [0] * len(self.punctuation)
         with open(file_path, "r") as file:
             for line in file:
-                words.extend(re.findall(self.wordRegex, line.lower()))
-                for i in range(len(self.punc_count)):
-                    self.punc_count[i] += line.count(self.punctuation[i])
+                words.extend(self.find_words(line))
+                self.collect_punctuation(line)
+        self.correct_ellipsis_count()
+        self.words = Counter(words)
+
+    def find_words(self, line) -> List[str]:
+        return re.findall(self.wordRegex, line.lower())
+
+    def collect_punctuation(self, line: str):
+        for i in range(len(self.punc_count)):
+            self.punc_count[i] += line.count(self.punctuation[i])
+
+    def correct_ellipsis_count(self):
         # Take into account for "." matching multiple times when "..." occurs
         self.punc_count[3] -= self.punc_count[2] * 3
-        self.words = Counter(words)
 
     def get_most_common(self, num_of_top_words=10) -> List[Tuple[str, int]]:
         return self.words.most_common(num_of_top_words)
 
-    def get_num_of_sentences(self):
+    def get_num_of_sentences(self) -> int:
         return sum(self.punc_count)
